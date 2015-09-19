a948f049bf7e0252c9c4f3fae9c8150205cfc31e
Taylor Huston <taylor.a.huston@gmail.com>
2015-09-18 19:10:37 -0700

    Outline for EdgeWeightGraph class

---
+++ EdgeWeightGraph.java
@@ -0,0 +1,32 @@
+/**
+ * Edge for a Edge-Weighted Graph
+ * Based on Algorithms, 4th Ed by Robert Sedgewick | Kevin Wayne
+ */
+public class EdgeWeightGraph {
+    int V;  //Number of vertices
+    int E;  //Number of edges
+
+    //EdgeWeightGraph(int V) {}
+
+    //EdgeWeightGraph(In in) {}
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    //Add a new edge
+    //void addEdge(Edge e) {}
+
+    //Edges adjacent to vertex v
+    //Iterable adj(int v) {}
+
+    //All of the edges
+    //Iterable edges() {}
+
+    //String toString() {}
+
+}
