cd7d8802804bb9746b18c1ef0e8a421998b85ccc
Taylor Huston <taylor.a.huston@gmail.com>
2015-09-18 20:08:08 -0700

    Fill in EdgeWeightGraph class

--- EdgeWeightGraph.java
+++ EdgeWeightGraph.java
@@ -3,12 +3,32 @@
  * Based on Algorithms, 4th Ed by Robert Sedgewick | Kevin Wayne
  */
 public class EdgeWeightGraph {
-    int V;  //Number of vertices
-    int E;  //Number of edges
+    private int V;              //Number of vertices
+    private int E;              //Number of edges
+    private Queue<Edge>[] adj;  //Array of lists of adjacent vertices
 
-    //EdgeWeightGraph(int V) {}
+    public EdgeWeightGraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Queue<Edge>[]) new Queue[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Queue<Edge>();
+        }
+    }
 
-    //EdgeWeightGraph(In in) {}
+    public EdgeWeightGraph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+
+        //Build all of the edges
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            double weight = in.readDouble();
+            Edge e = new Edge(v, w, weight);
+            addEdge(e);
+        }
+    }
 
     public int V() {
         return V;
@@ -19,14 +39,48 @@ public class EdgeWeightGraph {
     }
 
     //Add a new edge
-    //void addEdge(Edge e) {}
+    public void addEdge(Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        adj[v].enqueue(e);
+        adj[w].enqueue(e);
+        E++;
+    }
 
     //Edges adjacent to vertex v
-    //Iterable adj(int v) {}
+    public Iterable adj(int v) {
+        return adj[v];
+    }
 
     //All of the edges
-    //Iterable edges() {}
+   public Iterable edges() {
+       Queue<Edge> edges = new Queue<>();
+       for (int v = 0; v < V; v++) {
+           for (Edge e : adj[v]) {
+               if (e.other(v) > v) {
+                   edges.enqueue(e);
+               }
+           }
+       }
+       return edges;
+   }
 
-    //String toString() {}
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " " + E + "\n");
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (Edge e : adj[v]) {
+                s.append(e + "  ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightGraph G = new EdgeWeightGraph(in);
+        StdOut.println(G);
+    }
 }
